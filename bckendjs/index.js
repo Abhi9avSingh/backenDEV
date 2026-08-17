b4735611505105fb4d781b31a45523b12552d409
abhinav singh <abhinavsingh762006@gmail.com>
2026-08-17 16:41:22 +0530

    Added frontend and backend

---
+++ bckendjs/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.use(cors());
+ 
+
+app.get('/api', (req, res) => {
+    res.send('Server is working!');
+});
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "The Programmer",
+            content: "Why do programmers prefer dark mode? Because light attracts bugs!"
+        },
+        {
+            id: 2,
+            title: "The Developer",
+            content: "Why did the developer go broke? Because he used up all his cache!"
+        },
+        {
+            id: 3,
+            title: "The Developer",
+            content: "Why did the developer go broke? Because he used up all his cache!"
+        },
+        {
+            id: 4,
+            title: "The Developer",
+            content: "Why did the developer go broke? Because he used up all his cache!"
+        },
+        {
+            id: 5,
+            title: "The Developer",
+            content: "Why did the developer go broke? Because he used up all his cache!"
+        }
+    ];
+
+    res.send(jokes);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+ 
+ 
