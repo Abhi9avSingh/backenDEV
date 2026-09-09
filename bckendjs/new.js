1bd9b41a62e59984aa45697aa449a05e48b3bfa4
abhinav singh <abhinavsingh762006@gmail.com>
2026-09-09 15:57:13 +0530

    ajekj

--- bckendjs/new.js
+++ bckendjs/new.js
@@ -1,11 +1,11 @@
-import * as fs from 'fs/promises'
-try{
-    await fs.writeFile("demo.txt","hello")
-    (
-        "C:\\Users\\abhin\\OneDrive\\Apps\\code\\connectingfntndback\\bckendjsblankFolder"
-    );
-    console.log("Folder created successfully");
-} catch (error) {
-    console.error( error);
-}
+// import * as fs from 'fs/promises'
+// try{
+//     await fs.writeFile("demo.txt","hello")
+//     (
+//         "C:\\Users\\abhin\\OneDrive\\Apps\\code\\connectingfntndback\\bckendjsblankFolder"
+//     );
+//     console.log("Folder created successfully");
+// } catch (error) {
+//     console.error( error);
+// }
  
